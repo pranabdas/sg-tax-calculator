280c64b54f4bfa31ce02124a548098de3b5039d9
Pranab Das <31024886+pranabdas@users.noreply.github.com>
2024-10-16 08:14:34 +0800

    adjustments to base url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/sg-tax-calculator/",
+  base: "/",
   plugins: [react()],
   server: {
     port: 3000,
